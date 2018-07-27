d157afc5bf571dbb618c6ae4a659069cd44d488e
Rob Zienert <rzienert@netflix.com>
2018-07-27 14:26:44 -0700

    whole bunch of stuff I missed

--- dev/path.zsh
+++ dev/path.zsh
@@ -1,4 +1,4 @@
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export GROOVY_HOME=/usr/local/opt/groovy/libexec
 export GOPATH=~/Projects/go
-export PATH=$GROOVY_HOME/bin:$GOPATH/bin:$PATH
-
+export PATH=$GROOVY_HOME/bin:$GOPATH/bin:$HOME/.cargo/bin:$PATH
